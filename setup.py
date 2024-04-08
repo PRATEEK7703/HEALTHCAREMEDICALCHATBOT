4320821ddf73325408795e3c81209abafbe00fcf
PRATEEK7703 <prateeksutharwork@gmail.com>
2024-04-09 00:24:16 +0530

    MEDICAL CHATBOT

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Prateek Suthar',
+    author_email= 'prateeksuthar62@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
